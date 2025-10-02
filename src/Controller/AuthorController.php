1e3011dac9e4f87838330594493ee41d1878f932
ibrahim-zerzeri <ibrahim.zerzeri@gmail.com>
2025-10-02 17:25:23 +0200

    4eme seance creation de database and entiites

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AuthorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+
+    #[Route('/showauthor/{name}', name: 'app_show_author')]
+    public function showAuthor($name):  Response
+    {
+        return $this->render('author/show.html.twig', [
+            'nom' => $name,
+        ]); 
+    }
+
+    #[Route('/listauthors', name: 'app_list_authors')]
+    public function listAuthors():  Response
+    {
+        $authors = array(
+            array('id' => 1 , 'picture' => 'assets/images/1.jpg' , 'username' => 'Victor Hugo' , 'email' => 'victor.hugo@gmail.com' , 'nb_books' => 100),
+            array('id' => 2 , 'picture' => 'assets/images/2.jpg' , 'username' => 'William Shakespear' , 'email' => 'william.shakespear@gmail.com' , 'nb_books' => 200),
+            array('id' => 3 , 'picture' => 'assets/images/3.jpg' , 'username' => 'Taha Hussein' , 'email' => 'taha.hussein@gmail.com' , 'nb_books' => 300),
+        );
+
+
+        return $this->render('author/listauthors.html.twig',['authors' => $authors]
+        ); 
+    }
+
+    #[Route('/showAll', name: 'showAll')]
+    public function showAll(AuthorRepository $repo):  Response
+    {
+        $authors = $repo->findAll();
+
+        return $this->render('author/showAll.html.twig',['list' => $authors]
+        ); 
+    }
+
+}
